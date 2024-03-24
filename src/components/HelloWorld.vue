7cbf9cd0912892cedd078dbe2f9d3f6ee67d24f8
Jenny-Tany <1273368446@qq.com>
2024-03-24 10:40:48 +0800

    add drawer navigation bar

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-card>
+  <!-- <v-card>
     <v-toolbar
       color="primary"
     >
@@ -7,6 +7,93 @@
     </v-toolbar>
     
     <div class="d-flex flex-row">
+      <v-tabs
+        v-model="tab"
+        color="primary"
+        direction="vertical"
+      >
+        <v-tab value="option-1">
+          <v-icon start>
+            mdi-account
+          </v-icon>
+          用户
+        </v-tab>
+        <v-tab value="option-2">
+          <v-icon start>
+            <svg-icon type="mdi" :path="path"></svg-icon>
+          </v-icon>
+          上传图片
+        </v-tab>
+        <v-tab value="option-3">
+          <v-icon start>
+            <svg-icon type="mdi" :path="path1"></svg-icon>
+          </v-icon>
+          历史记录
+        </v-tab>
+        <v-tab value="option-4">
+          <v-icon start>
+            <svg-icon type="mdi" :path="path2"></svg-icon>
+          </v-icon>
+          任务日历
+        </v-tab>
+      </v-tabs>
+      <v-window v-model="tab">
+        <v-window-item value="option-1">
+          <User />
+        </v-window-item>
+        <v-window-item value="option-2">
+          <UpLoadFiles />
+          <div style="text-align: center;">
+          </div>
+        </v-window-item>
+        <v-window-item value="option-3">
+          <DataQuery />
+        </v-window-item>
+        <v-window-item value="option-4">
+          <WorkList />
+        </v-window-item>
+      </v-window>
+    </div>
+  </v-card> -->
+
+  <v-card>
+    <v-toolbar
+      color="primary"
+    >
+      <v-toolbar-title>井盖检测</v-toolbar-title>
+    </v-toolbar>
+    <v-layout>
+      
+      <v-navigation-drawer
+        v-model="drawer"
+        :rail="rail"
+        permanent
+        @click="toggleRail1"
+      >
+        <v-list-item
+          prepend-avatar="https://randomuser.me/api/portraits/men/85.jpg"
+          title="John user"
+          nav
+        >
+          <template v-slot:append>
+            <v-btn
+              icon="mdi-chevron-left"
+              variant="text"
+              @click.stop="toggleRail()"
+            ></v-btn>
+          </template>
+        </v-list-item>
+
+        <v-divider></v-divider>
+
+        <!-- <v-list density="compact" nav @click="selectComponent">
+          <v-list-item prepend-icon="mdi-account" title="My Account" value="account"></v-list-item>
+          <v-list-item prepend-icon="mdi-upload" title="上传图片" value="files"></v-list-item>
+          <v-list-item prepend-icon="mdi-home-city" title="Home" value="home"></v-list-item>
+          <v-list-item prepend-icon="mdi-history" title="历史记录" value="history"></v-list-item>
+
+        </v-list> -->
+        <div class="d-flex flex-row">
       <v-tabs
         v-model="tab"
         color="primary"
@@ -40,7 +127,11 @@
           任务日历
         </v-tab>
       </v-tabs>
-      <v-window v-model="tab">
+
+    </div>
+      </v-navigation-drawer>
+      <v-main :style="{ '--v-layout-left': mainStyle }">
+        <v-window v-model="tab">
         <v-window-item value="option-1">
           <User />
         </v-window-item>
@@ -61,13 +152,16 @@
           <!-- <Map /> -->
         </v-window-item>
       </v-window>
-    </div>
+      </v-main>
+    </v-layout>
   </v-card>
 </template>
 
 <script setup>
 import test from '@/pages/test.vue';
 import DataQuery from '@/components/DataQuery.vue';
+import User from '@/components/User.vue'
+import UpLoadFiles from '@/components/UpLoadFiles.vue'
 
 // 引入图标
 import SvgIcon from '@jamescoyle/vue-icon';
@@ -77,6 +171,30 @@ const path1 = mdiHistory;
 const path2 = mdiFormatListBulleted;
 
 const tab = ref('option-1');
+
+// 抽屉式导航
+const drawer = ref(true);
+const rail = ref(true);
+const selectedComponent = ref(User)
+const selectComponent = (value) => {
+  if (value === 'account') {
+    selectedComponent.value = User;
+  } else if (value === 'files') {
+    selectedComponent.value = UpLoadFiles;
+  }
+};
+const mainStyle = ref('148px');
+
+const toggleRail = () => {
+  // mainStyle.value = mainStyle.value === '148px' ? '5px' : '148px';
+  mainStyle.value = '5px';
+  rail.value = !rail.value;
+};
+const toggleRail1 = () => {
+  // mainStyle.value = mainStyle.value === '148px' ? '5px' : '148px';
+  mainStyle.value = '148px';
+  rail.value = false;
+};
 </script>
 
 <style lang="scss" scoped>
@@ -93,4 +211,11 @@ const tab = ref('option-1');
   .v-window {
     margin: 0 0 5vh 3vh;
   }
+  .v-navigation-drawer {
+    width: 156px !important;
+  }
+  .v-navigation-drawer--rail {
+    width: 40px !important;
+  }
+  
 </style>
